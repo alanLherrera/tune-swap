1dd3429ad9ae95817e8566902ca35bb7f7f9c2fd
natdagr85 <nathanmansoor1128@gmail.com>
2023-03-27 18:01:38 -0700

    added some styling changes

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,17 +1,19 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import './App.css';
 
 function NavBar() {
   return (
     <>
-      <Navbar sticky="top" bg="dark" variant="dark">
+      <Navbar className='Nav' sticky="top" variant="dark">
         <Container>
           <Navbar.Brand href="#home">Tune-Swap</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#features">Features</Nav.Link>
             <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link href="#sign-in">Sign-in</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
