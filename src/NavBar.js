64db4d3b3bf557620915f9b05a5d16412e150f52
natdagr85 <nathanmansoor1128@gmail.com>
2023-03-27 17:40:12 -0700

    started home page, added react bootstrap and Navbar

---
+++ src/NavBar.js
@@ -0,0 +1,22 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+function NavBar() {
+  return (
+    <>
+      <Navbar sticky="top" bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Tune-Swap</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
+  );
+}
+
+export default NavBar;
