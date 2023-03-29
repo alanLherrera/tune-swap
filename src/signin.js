4462cd234f9d5a06bd7dfaadece89db86dd43e83
natdagr85 <nathanmansoor1128@gmail.com>
2023-03-28 19:17:20 -0700

    added card and text with links to sign in page

--- src/signin.js
+++ src/signin.js
@@ -1,4 +1,4 @@
-import { Card } from "react-bootstrap"
+import { Card, Button } from "react-bootstrap"
 
 function SignIn () {
     return(
@@ -6,7 +6,17 @@ function SignIn () {
             <h1>Sign-In Page</h1>
             <br></br>
             <Card className="navCard">
-                <Card.Body>"hello world"</Card.Body>
+                <Card.Body>
+                    <Card.Title className="cardTitle">Sign in to Spotify</Card.Title>
+                    <Card.Body>
+                        <Card.Text>In order to use this application, we are required to have you sign in with Spotify</Card.Text>
+                        <br></br>
+                        <Button variant="success">Sign-in</Button>{' '}
+                        <br></br>
+                        <br></br>
+                        <Card.Link className="createAcc" target="_blank" rel="noopener noreferrer" href='https://www.spotify.com/us/signup?forward_url=https%3A%2F%2Fopen.spotify.com%2F'>Create Spotify account here</Card.Link>
+                    </Card.Body>
+                </Card.Body>
             </Card>
         </div>
     )
