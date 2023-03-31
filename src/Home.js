2aa7bbad98e72332fa2a78b4388df560753c9ded
Ezequiel Capaceta <ezequielcapaceta@Ezequiels-MacBook-Pro.local>
2023-03-30 19:17:45 -0700

    first rough draft for the idea of the home page

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,22 @@
+import { Navbar } from "react-bootstrap"
+
 function Home () {
     return(
         <>
-            <h1>This is Tune Swap</h1>
+        <div class="header">
+        <h1 class="header">This is Tune Swap</h1>
+            <h2 class="short-desc">A playlist just for you</h2>
+         
+        </div>
+        <body>
+        <div class="container" >
+               <div class="item item-1">Small description of how the app works and a link that sends you to the Sign-In page</div>
+               <div class="item item-2">The actual generate button</div>
+               <div class="item item-3">Music playlist logos</div>
+            </div>
+        </body>
+          
         </>
-
     )
 }
 
