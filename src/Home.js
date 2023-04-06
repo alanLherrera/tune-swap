9b282dd0729a9cf35ec80859ecf882b254e03f0e
natdagr85 <nathanmansoor1128@gmail.com>
2023-04-05 19:19:22 -0700

    nathans merge

--- src/Home.js
+++ src/Home.js
@@ -3,16 +3,16 @@ import { Navbar } from "react-bootstrap"
 function Home () {
     return(
         <>
-        <div class="header">
-        <h1 class="header">This is Tune Swap</h1>
-            <h2 class="short-desc">A playlist just for you</h2>
+        <div className="header">
+        <h1 className="header">This is Tune Swap</h1>
+            <h2 className="short-desc">A playlist just for you</h2>
          
         </div>
         <body>
-        <div class="container" >
-               <div class="item item-1">Small description of how the app works and a link that sends you to the Sign-In page</div>
-               <div class="item item-2">The actual generate button</div>
-               <div class="item item-3">Music playlist logos</div>
+        <div className="container" >
+               <div className="item item-1">Small description of how the app works and a link that sends you to the Sign-In page</div>
+               <div className="item item-2">The actual generate button</div>
+               <div className="item item-3">Music playlist logos</div>
             </div>
         </body>
           
